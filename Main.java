6921d44af54df3cf78207d24f3bf3a6781dda39b
Umezawa Toshifumi <bday7125@gmail.com>
2017-01-20 18:57:55 +0900

    最コミット

--- Main.java
+++ Main.java
@@ -4,14 +4,8 @@ import functions.*;
 
 class Main{
 	public static void main(String[] args) {
-		ReferenceVariable<Double> p = new ReferenceVariable<Double>(0.5);
-		ReferenceVariable<Double> p2 = new ReferenceVariable<Double>(p);
-		System.out.println(p.get());
-		System.out.println(p2.get());
-		p.set(1.5);
-		System.out.println(p.get());
-		System.out.println(p2.get());
-		Data.set = XmlUtil.read("save.svg");
+		if(args.length==0) Data.set = XmlUtil.read("save.svg");
+		else Data.set = XmlUtil.read(args[0]);
 		MainFrame mf = new MainFrame();
 	}
 }
